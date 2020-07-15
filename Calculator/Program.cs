772432641ad9f27fd2204da2f6897ea62afc4276
Jeremy Chionglo <jpchionglo@gmail.com>
2020-07-15 15:33:47 -0500

    added calculator project, modified hellocsharp

---
+++ Calculator/Program.cs
@@ -0,0 +1,103 @@
+﻿namespace Calculator
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+
+      System.Console.WriteLine("Welcome to our basic calculator!");
+      var stay = true;
+
+      do {
+
+        System.Console.WriteLine("Menu");
+        System.Console.WriteLine("Press 1 for Addition");
+        System.Console.WriteLine("Press 2 for Subtraction");
+        System.Console.WriteLine("Press 3 for Multiplication");
+        System.Console.WriteLine("Press 4 for Division");
+        System.Console.WriteLine("Press 5 to exit");
+
+        var input = System.Console.ReadLine();
+
+        switch (input)
+        {
+
+          case "1":
+            //var input1 = (double)System.Console.ReadLine(); //explicit casting with exception on fail
+            //var input2 = System.Console.ReadLine() as double; //explicit casting with null on fail
+
+            var input1 = double.Parse(System.Console.ReadLine()); //explicit parsing with 0 on fail
+            double input2;
+            double.TryParse(System.Console.ReadLine(), out input2); //explicit parsing with exception on fail
+
+            Add(input1, input2);
+
+            break;
+
+          case "2":
+
+            var input3 = double.Parse(System.Console.ReadLine()); 
+            double input4;
+            double.TryParse(System.Console.ReadLine(), out input4);
+
+            Subtract(input3, input4);
+
+            break;
+
+          case "3":
+
+            var input5 = double.Parse(System.Console.ReadLine()); 
+            double input6;
+            double.TryParse(System.Console.ReadLine(), out input6);
+
+            Multiply(input5, input6);
+
+            break;
+
+          case "4":
+
+            var input7 = double.Parse(System.Console.ReadLine()); 
+            double input8;
+            double.TryParse(System.Console.ReadLine(), out input8);
+            Divide(input7, input8); //handle divide by 0 (loop/throw an exception), division by 0 returns infinity
+
+            break;
+
+          case "5":
+          
+            stay = false;
+            break;
+
+        }
+
+      } while (stay);
+
+    }
+
+    static void Add(double operand1, double operand2)
+    {
+
+      System.Console.WriteLine($"{operand1} + {operand2} = {operand1 + operand2}");
+
+    }
+
+    static void Subtract(double operand1, double operand2){
+
+      System.Console.WriteLine($"{operand1} - {operand2} = {operand1 - operand2}");
+
+    }
+
+    static void Multiply(double operand1, double operand2){
+
+      System.Console.WriteLine($"{operand1} * {operand2} = {operand1 * operand2}");
+
+    }
+
+    static void Divide(double operand1, double operand2){
+
+      System.Console.WriteLine($"{operand1} / {operand2} = {operand1 / operand2}");
+
+    }
+
+  }
+}
--- HelloCSharp/Program.cs
+++ HelloCSharp/Program.cs
@@ -4,9 +4,18 @@ namespace HelloCSharp
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            Console.WriteLine("Hello World!");
+            Console.Write("Enter your first name: \n");
+            var firstName = Console.ReadLine(); // lazy, data type is given based on content
+            Console.Write("Enter your last name: \n");
+            string lastName = Console.ReadLine(); // eager, data type is explicit prior to content
+
+            //Console.WriteLine(firstName + " " + lastName);
+            //Console.WriteLine("{0} {1}", firstName, lastName);
+            Console.WriteLine($"{firstName} {lastName}");
+
+            Console.ReadLine();
         }
     }
 }
